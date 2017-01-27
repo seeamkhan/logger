8a00d9b3e102187b3baf81b895c62a7ffbe655ba
seeamkhan <zafi005@gmail.com>
2017-01-27 13:00:52 +0600

    add new method

--- logger.py
+++ logger.py
@@ -18,3 +18,13 @@ target = open(file_path, 'a')
 # target.truncate()
 target.write("\n\n%s" % log)
 target.close()
+
+# You can also use this method directly
+def write_error_log(Error):
+    """
+Error parameter should contains the string of the error message when the file could not open.
+    :param Error:
+    """
+    target = open("Error_log.txt", 'a')
+    target.write(Error)
+    target.close()
